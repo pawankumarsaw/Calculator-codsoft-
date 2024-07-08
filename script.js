ab374eecc30e27685492aeec06735b4fd09fefd1
Pawan Kumar <pk656527@gmail.com>
2024-07-08 14:56:15 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,29 @@
+let display = document.getElementById('display');
+
+function clearDisplay() {
+    display.innerText = '0';
+}
+
+function deleteLast() {
+    if (display.innerText.length > 1) {
+        display.innerText = display.innerText.slice(0, -1);
+    } else {
+        display.innerText = '0';
+    }
+}
+
+function appendCharacter(character) {
+    if (display.innerText === '0') {
+        display.innerText = character;
+    } else {
+        display.innerText += character;
+    }
+}
+
+function calculateResult() {
+    try {
+        display.innerText = eval(display.innerText);
+    } catch {
+        display.innerText = 'Error';
+    }
+}
